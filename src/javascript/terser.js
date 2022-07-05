1654f3bb2d86670d3171be2928a0ac3f96023a3e
nluciano233 <nluciano233@gmail.com>
2022-07-05 12:30:28 +0200

    fte: configured so it minifies files individually and not in a single main file

--- src/javascript/terser.js
+++ src/javascript/terser.js
@@ -3,34 +3,36 @@
 const fs = require("fs");
 const { minify } = require("terser");
 
-const files = ["plane-wave.js"]; // put inside here all the files that you want to uglify
+const files = ["plane-wave.js", "sphere.js"]; // put inside here all the files that you want to uglify
 
-// read all the content from the files and organize them into a single string
+// read all the content from the files and return them into an array
 function readFiles (files) {
   let data = [];
   files.forEach(e => {
     data.push(fs.readFileSync(require('path').resolve(__dirname, e)).toString());
   })
 
-  data = data.join(' ');
   return data;
 }
 
 // pass the string with all the code into the uglifier and then write it to a file
-async function uglify() {
+async function uglify(files) {
   const filesData = await readFiles(files);
+  
+  filesData.forEach( async (e, i) => {
 
-  var code = {
-    "main.js": `${filesData}`
-  };
+    var code = {};
+    code[files[i]] = `${e}`
 
-  var result = await minify(code);
+    var result = await minify(code);
 
-  fs.writeFile(`${__dirname}/../../public/javascripts/main.js`, result.code, function(err) {
-    if (err) {
-      console.log(err);
-    }
+    fs.writeFile(`${__dirname}/../../public/javascripts/${files[i]}`, result.code, function(err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   });
+
 };
 
-uglify();
+uglify(files);
